c8b37527e0ac59b162ff50f84d55cec5acc3a68b
alicettins <111758338+alicettins@users.noreply.github.com>
2023-10-17 19:55:21 -0300

    Laboratório de Banco de Dados - Entrega 2
    ANA ALICE ALVES MARTINS
    JOÃO VICTOR DE JESUS ALVES

---
+++ criacao_populacao.sql.sql
@@ -0,0 +1,290 @@
+CREATE DATABASE  IF NOT EXISTS `anaalice_joao` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `anaalice_joao`;
+-- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
+--
+-- Host: localhost    Database: anaalice_joao
+-- ------------------------------------------------------
+-- Server version	8.0.32
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `campus`
+--
+
+DROP TABLE IF EXISTS `campus`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `campus` (
+  `id` int NOT NULL,
+  `cidade_id` int DEFAULT NULL,
+  `nome` varchar(255) DEFAULT NULL,
+  `endereco` varchar(255) DEFAULT NULL,
+  `telefone` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `campus`
+--
+
+LOCK TABLES `campus` WRITE;
+/*!40000 ALTER TABLE `campus` DISABLE KEYS */;
+INSERT INTO `campus` VALUES (301,401,'Campus A','Rua 1, Cidade A','123-456-7890','campusa@email.com'),(302,402,'Campus B','Avenida 2, Cidade B','987-654-3210','campusb@email.com');
+/*!40000 ALTER TABLE `campus` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cidade`
+--
+
+DROP TABLE IF EXISTS `cidade`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cidade` (
+  `id` int NOT NULL,
+  `uf_id` int DEFAULT NULL,
+  `nome` varchar(255) DEFAULT NULL,
+  `populacao` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cidade`
+--
+
+LOCK TABLES `cidade` WRITE;
+/*!40000 ALTER TABLE `cidade` DISABLE KEYS */;
+INSERT INTO `cidade` VALUES (401,1,'Cidade A',100000),(402,2,'Cidade B',150000),(403,3,'Cidade C',80000);
+/*!40000 ALTER TABLE `cidade` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `disciplina`
+--
+
+DROP TABLE IF EXISTS `disciplina`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `disciplina` (
+  `id` int NOT NULL,
+  `nome` varchar(255) DEFAULT NULL,
+  `codigo` varchar(255) DEFAULT NULL,
+  `carga_horaria` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `disciplina`
+--
+
+LOCK TABLES `disciplina` WRITE;
+/*!40000 ALTER TABLE `disciplina` DISABLE KEYS */;
+INSERT INTO `disciplina` VALUES (101,'Matemática','MAT01',60),(102,'Física','FIS01',45),(103,'Química','QUI01',50),(104,'História','HIS01',40),(105,'Inglês','ING01',30);
+/*!40000 ALTER TABLE `disciplina` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `estudante`
+--
+
+DROP TABLE IF EXISTS `estudante`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `estudante` (
+  `id` int NOT NULL,
+  `cidade_id` int DEFAULT NULL,
+  `nome` varchar(255) DEFAULT NULL,
+  `endereco` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `estudante`
+--
+
+LOCK TABLES `estudante` WRITE;
+/*!40000 ALTER TABLE `estudante` DISABLE KEYS */;
+INSERT INTO `estudante` VALUES (301,401,'Estudante 1','Endereço 1'),(302,402,'Estudante 2','Endereço 2'),(303,403,'Estudante 3','Endereço 3'),(304,401,'Estudante 4','Endereço 4'),(305,402,'Estudante 5','Endereço 5');
+/*!40000 ALTER TABLE `estudante` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `professor`
+--
+
+DROP TABLE IF EXISTS `professor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `professor` (
+  `id` int NOT NULL,
+  `cidade_id` int DEFAULT NULL,
+  `titulacao_id` int DEFAULT NULL,
+  `nome` varchar(255) DEFAULT NULL,
+  `matricula` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `professor`
+--
+
+LOCK TABLES `professor` WRITE;
+/*!40000 ALTER TABLE `professor` DISABLE KEYS */;
+INSERT INTO `professor` VALUES (201,401,501,'João','MAT101','joao@email.com'),(202,402,502,'Maria','FIS102','maria@email.com'),(203,401,501,'Pedro','QUI103','pedro@email.com'),(204,402,503,'Ana','HIS104','ana@email.com'),(205,403,504,'Luís','ING105','luis@email.com');
+/*!40000 ALTER TABLE `professor` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `titulacao`
+--
+
+DROP TABLE IF EXISTS `titulacao`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `titulacao` (
+  `id` int NOT NULL,
+  `descricao` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `titulacao`
+--
+
+LOCK TABLES `titulacao` WRITE;
+/*!40000 ALTER TABLE `titulacao` DISABLE KEYS */;
+INSERT INTO `titulacao` VALUES (501,'Mestre'),(502,'Doutor'),(503,'Especialista'),(504,'Bacharel'),(505,'Sem titulação');
+/*!40000 ALTER TABLE `titulacao` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `turma`
+--
+
+DROP TABLE IF EXISTS `turma`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `turma` (
+  `id` int NOT NULL,
+  `disciplina_id` int DEFAULT NULL,
+  `professor_id` int DEFAULT NULL,
+  `campus_id` int DEFAULT NULL,
+  `turno_id` int DEFAULT NULL,
+  `observacao_id` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `turma`
+--
+
+LOCK TABLES `turma` WRITE;
+/*!40000 ALTER TABLE `turma` DISABLE KEYS */;
+INSERT INTO `turma` VALUES (1,101,201,301,1,'Turma A'),(2,102,202,302,2,'Turma B'),(3,103,203,301,3,'Turma C'),(4,104,204,302,4,'Turma D'),(5,105,205,301,1,'Turma E');
+/*!40000 ALTER TABLE `turma` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `turma_estudante`
+--
+
+DROP TABLE IF EXISTS `turma_estudante`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `turma_estudante` (
+  `id` int NOT NULL,
+  `turma_id` int DEFAULT NULL,
+  `estudante_id` int DEFAULT NULL,
+  `data_matricula` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `turma_estudante`
+--
+
+LOCK TABLES `turma_estudante` WRITE;
+/*!40000 ALTER TABLE `turma_estudante` DISABLE KEYS */;
+INSERT INTO `turma_estudante` VALUES (1,1,301,'2023-09-01'),(2,1,302,'2023-09-02'),(3,2,303,'2023-09-03'),(4,2,304,'2023-09-04'),(5,3,305,'2023-09-05');
+/*!40000 ALTER TABLE `turma_estudante` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `turno`
+--
+
+DROP TABLE IF EXISTS `turno`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `turno` (
+  `id` int NOT NULL,
+  `descricao` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `turno`
+--
+
+LOCK TABLES `turno` WRITE;
+/*!40000 ALTER TABLE `turno` DISABLE KEYS */;
+INSERT INTO `turno` VALUES (1,'Manhã'),(2,'Tarde'),(3,'Noite'),(4,'Integral'),(5,'Personalizado');
+/*!40000 ALTER TABLE `turno` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `uf`
+--
+
+DROP TABLE IF EXISTS `uf`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `uf` (
+  `id` int NOT NULL,
+  `nome` varchar(255) DEFAULT NULL,
+  `sigla` varchar(2) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `uf`
+--
+
+LOCK TABLES `uf` WRITE;
+/*!40000 ALTER TABLE `uf` DISABLE KEYS */;
+INSERT INTO `uf` VALUES (1,'Estado A','EA'),(2,'Estado B','EB'),(3,'Estado C','EC');
+/*!40000 ALTER TABLE `uf` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-10-17 10:02:56
